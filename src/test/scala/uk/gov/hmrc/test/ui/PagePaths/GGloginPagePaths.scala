9093787b712c6508223831215b58ccec5c4073ee
kanchan555-ui <kanchan.paliwal@digital.hmrc.gov.uk>
2023-03-03 10:10:26 +0000

    feedback tc fixed

--- src/test/scala/uk/gov/hmrc/test/ui/PagePaths/GGloginPagePaths.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/PagePaths/GGloginPagePaths.scala
@@ -40,7 +40,7 @@ trait SCAStartPagePaths {
   val scaMessage                 = "//span[contains(text(),'Reminder to file a Self Assessment return')]"
   val yourMessage                = "Reminder to file a Self Assessment return"
   val backButton                 = "govuk-back-link"
-  val Feedbacklink               = "./html/body/div/main/div/div/div[2]/div/p/span/a"
+  val Feedbacklink               = "//a[contains(text(),‘feedback’)]"
   val SCAAccessErrorText         = "govuk-heading-xl"
   val BusinessAccountLink        = "//*[contains(text(),'Business tax account')]"
   val AccountHomeLink            = "(//*[contains(text(),'Account home')])[1]"
