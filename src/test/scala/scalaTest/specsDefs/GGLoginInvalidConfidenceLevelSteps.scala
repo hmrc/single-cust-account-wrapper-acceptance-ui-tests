7f1b776f6a85c17545d3d98c460e3d8d3c7c966e
dannybassi <72074461+dannybassi@users.noreply.github.com>
2025-11-20 18:06:18 +0000

    clean up part 3

--- src/test/scala/scalaTest/specsDefs/GGLoginInvalidConfidenceLevelSteps.scala
+++ src/test/scala/scalaTest/specsDefs/GGLoginInvalidConfidenceLevelSteps.scala
@@ -21,20 +21,20 @@ import uk.gov.hmrc.test.ui.pages.{GGLoginPage, GGLoginPageInvalidConfidenceLevel
 object GGLoginInvalidConfidenceLevelSteps {
 
   // User login to the GGLogin Page$
-  def givenUserLoginToTheGGLoginPage(): Unit = {
+  def userLoginToTheGGLoginPage(): Unit = {
     GGLoginPage.navigateToAuthLoginStub()
         GGLoginPage.enterRedirectURL()
   }
 
   // ^Confidence level less is less than 200$
-  def whenConfidenceLevelLessIsLessThan200(): Unit = {
+  def confidenceLevelLessThan200(): Unit = {
     GGLoginPageInvalidConfidenceLevel.selectConfidenceLevel()
         GGLoginPage.enterNino()
         GGLoginPage.clickSubmitButton()
   }
 
   // ^User should see SCA home page with an error "([^"]*)"$
-  def thenUserShouldSeeSCAHomePageWithAnError()(accessError: String): Unit = {
+  def SCAHomePageError()(accessError: String): Unit = {
     assert(GGLoginPageInvalidConfidenceLevel.verifySCAStartPageAccessError(accessError))
   }
 
